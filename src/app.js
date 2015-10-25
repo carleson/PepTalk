6b711368e8ad9634a530c3dec7ca547277b55638
Johan Carleson <johan.carleson@gmail.com>
2015-10-25 10:52:05 +0100

    PopTalk will now generate random words.

--- src/app.js
+++ src/app.js
@@ -9,19 +9,27 @@ var Vector2 = require('vector2');
 var Accel = require('ui/accel');
 var Vibe = require('ui/vibe');
 var PepTalks = [
-  "You are great!",
-  "You can do it!",
-  "You have amazing potential",
-  "You can do it!",
-  "You have amazing potential",
-  "You may not be there yet but you are closer than you was yesterday",
-  "This will not kill you",
-  "You are one tough bitch. Now go prove it",
-  "Believe in yourself, you are great",
-  "You can always Do More than you think you can",
-  "Small chenges eventually add up to huge results",
-  "Be the type of person you want to meet",
-  "You have not failed until you quit trying"
+  "#1. You are great!",
+  "#2. You can do it!",
+  "#3. You have amazing potential",
+  "#4. You can do it!",
+  "#5. You have amazing potential",
+  "#6. You may not be there yet but you are closer than you was yesterday",
+  "#7. This will not kill you",
+  "#8. You are one tough bitch. Now go prove it",
+  "#9. Believe in yourself, you are great",
+  "#10. You can always Do More than you think you can",
+  "#11. Small changes eventually add up to huge results",
+  "#12. Be the type of person you want to meet",
+  "#13. You have not failed until you quit trying",
+  "#14. In the middle of every difficulty lies opportunity",
+  "#15. The best revenge is massive success",
+  "#16. This too shall pass",
+  "#17. It doesn’t matter who you are, where you come from. The ability to triumph begins with you.",
+  "#18. Trust yourself. You know more than you think you do",
+  "#19. Live the life you have imagined",
+  "#20. Life is 10 percent what happens to me and 90 percent how I react to it",
+  "#21. We cannot direct the wind but we can adjust the sails"
 ];
 var Page = 0;
 
@@ -40,19 +48,29 @@ main.on('click', 'select', function(e) {
 
 main.on('accelTap', function(e) {
    Vibe.vibrate('short');
-   main.body(GetPepTalk());   
+   main.body(GetPepTalk()); 
   });
 
-function GetPepTalk(){
-  var text = PepTalks[Page];
-  if (Page < PepTalks.length)
-    {Page = Page+1;}
-  else
-    {Page = 0;}
-  
-  return text;
+function GetPepTalk() {
+  return PepTalks[GetRandomPage()];
+}
+
+function GetRandomPage(){
+  var number = 0;
+  do {
+       number = Math.floor((Math.random() * PepTalks.length-1) + 1);
+     }
+  while (number == Page);
+  Page = number;  
+  return number;
 }
 
 // Prepare the accelerometer
 Accel.init();
+
+//Next Features
+//Settings - Background Color,Text color, Text style, Numbers 
+//Splashscreen
+//Icon
+//More texts
 

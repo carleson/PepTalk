548a2e26b0cf32ff5d32532936124a214561bef6
Johan Carleson <johan.carleson@gmail.com>
2016-01-03 22:40:45 +0100

    Version 0.5 builed from the Gandhi source

--- src/app.js
+++ src/app.js
@@ -1,76 +1,222 @@
 /**
- * Welcome to Pebble.js!
+ * Words of Gandhi
  *
- * This is where you write your app.
+ * Source: https://github.com/carleson/Gandhi
+ *
+ * This is a simple quote appliaction with random Mahatma Gandhi quotes.
+ * http://www.brainyquote.com/quotes/authors/m/mahatma_gandhi.html
+ *
  */
 
+// DEBUGGING
+var DEBUG = true;
+var VERSION = 0.5;
+
+// INCLUDES
 var UI = require('ui');
 var Vector2 = require('vector2');
 var Accel = require('ui/accel');
 var Vibe = require('ui/vibe');
-var PepTalks = [
-  "#1. You are great!",
-  "#2. You can do it!",
-  "#3. You have amazing potential",
-  "#4. You can do it!",
-  "#5. You have amazing potential",
-  "#6. You may not be there yet but you are closer than you was yesterday",
-  "#7. This will not kill you",
-  "#8. You are one tough bitch. Now go prove it",
-  "#9. Believe in yourself, you are great",
-  "#10. You can always Do More than you think you can",
-  "#11. Small changes eventually add up to huge results",
-  "#12. Be the type of person you want to meet",
-  "#13. You have not failed until you quit trying",
-  "#14. In the middle of every difficulty lies opportunity",
-  "#15. The best revenge is massive success",
-  "#16. This too shall pass",
-  "#17. It doesn’t matter who you are, where you come from. The ability to triumph begins with you.",
-  "#18. Trust yourself. You know more than you think you do",
-  "#19. Live the life you have imagined",
-  "#20. Life is 10 percent what happens to me and 90 percent how I react to it",
-  "#21. We cannot direct the wind but we can adjust the sails"
-];
-var Page = 0;
+var Settings = require('settings');
 
-var main = new UI.Card({
-  title: 'PepTalk',
-  icon: 'images/menu_icon.png',
-  subtitle: '',
-  body: GetPepTalk()
+// GLOBAL VARIABLES
+var main;
+var default_text_color = "#000000";
+var default_background_color = "#ffffff"
+var config_background_color;
+var config_text_color;
+var config_splashscreen = true;
+var config_shake = true;
+var splashscreenTimer = 0;
+var Page = 0;
+var colorHex;
+var PepTalks = [
+  "You are great!",
+  "You can do it!",
+  "You have amazing potential",
+  "You can do it!",
+  "You have amazing potential",
+  "You may not be there yet but you are closer than you was yesterday",
+  "This will not kill you",
+  "You are one tough bitch. Now go prove it",
+  "Believe in yourself, you are great",
+  "You can always Do More than you think you can",
+  "Small changes eventually add up to huge results",
+  "Be the type of person you want to meet",
+  "You have not failed until you quit trying",
+  "In the middle of every difficulty lies opportunity",
+  "The best revenge is massive success",
+  "This too shall pass",
+  "Trust yourself. You know more than you think you do",
+  "Live the life you have imagined",
+  "Life is 10 percent what happens to me and 90 percent how I react to it",
+  "We cannot direct the wind but we can adjust the sails",
+  "You miss 100% of the shots you don’t take",
+  "Every strike brings me closer to the next home run",
+  "Eighty percent of success is showing up",
+  "Your time is limited, so don’t waste it living someone else’s life",
+  "Winning isn’t everything, but wanting to win is.",
+  "You are a product of your decisions",
+  "Whether you think you can or you think you can’t, you’re right"
+];
+
+
+// EVENT LISTENERS
+Pebble.addEventListener('ready', function() {
+  console.log('PebbleKit JS ready!');
 });
 
-main.show();
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'http://carleson.github.io/Pebble/Peptalk/index.html';
+  console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  if(DEBUG) console.log('Configuration page returned: ' + JSON.stringify(configData));
+
+  //Save configuration
+  Settings.data({
+  name: 'Peptalk',
+  splashscreen: configData['splashscreen'],
+  shake: configData['shake'], 
+  background_color:   configData['background_color'],
+  text_color:   configData['text_color'],
+  });
+  
+  if(DEBUG) displayConfig();
+  LoadConfiguration();
+  GetNewCard();
+});
+
+// CONFIGURATION
+LoadConfiguration();
+
+// WINDOWS
+var wind = new UI.Window({ fullscreen: true });
+var image = new UI.Image({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  image: 'images/Peptalk.png'
+});
+
+if (config_splashscreen === true)
+{
+  if(DEBUG) console.log( "Display SplashScreen..." );
+  wind.add(image);
+  wind.show();
+  splashscreenTimer = 2000;
+}
+
+  main = new UI.Card({
+  title: 'Peptalk',
+  icon: 'images/Peptalk28.png',
+  subtitle: '',
+  scrollable: true,
+  backgroundColor: config_background_color,
+  bodyColor: config_text_color,
+  body: GetQuote()
+});
+
+setTimeout(function() {
+  // Display the mainScreen
+  main.show();
+  // Hide the splashScreen to avoid showing it when the user press Back.
+  if (config_splashscreen === true)
+  {
+    wind.hide();
+  }
+}, splashscreenTimer);
 
 main.on('click', 'select', function(e) {
-  main.body(GetPepTalk());
+  GetNewCard();
   });
 
 main.on('accelTap', function(e) {
-   Vibe.vibrate('short');
-   main.body(GetPepTalk()); 
+   if (config_shake)
+     {
+       Vibe.vibrate('short');
+       GetNewCard();
+     }
   });
 
+main.on('longSelect', function(e) {
+  if(DEBUG) displayConfig();
+});
+
+// UTILITY FUNCTIONS
+
+function LoadConfiguration()
+{
+  if(DEBUG) console.log( "Method call: LoadConfiguration()" );
+  config_splashscreen = Settings.data('splashscreen');
+  config_shake = Settings.data('shake');
+  config_background_color = parseColor(Settings.data('background_color'));
+  if (config_background_color == '') config_background_color = default_background_color
+  
+  config_text_color = parseColor(Settings.data('text_color'));
+  if (config_text_color == '') config_text_color = default_text_color;
+}
+
+function GetNewCard() {
+  if(DEBUG) console.log( "Method call: GetNewCard()" );
+  main.body(GetPepTalk());
+  main.backgroundColor(config_background_color);
+  main.bodyColor(config_text_color);
+}
+
 function GetPepTalk() {
+  if(DEBUG) console.log( "Method call: GetPepTalk()" );
   return PepTalks[GetRandomPage()];
 }
 
 function GetRandomPage(){
+    if(DEBUG) console.log( "Method call: GetRandomPage()" );
   var number = 0;
   do {
-       number = Math.floor((Math.random() * PepTalks.length-1) + 1);
+       number = Math.floor((Math.random() * Quotes.length-1) + 1);
      }
   while (number == Page);
   Page = number;  
   return number;
 }
 
+function parseColor(color) {
+  if(DEBUG) console.log("Method call: parseColor()");
+  if(DEBUG) console.log(color); 
+  var fixed_color = '';
+  if (typeof(color) != 'undefined' && color != null)
+    {
+      fixed_color = '#' + color.toString().slice(2);
+      if(DEBUG) console.log(fixed_color);      
+    }
+  if(DEBUG) console.log( "Converting color: " + color + "into: " + fixed_color);
+	return fixed_color;
+};
+
+function displayConfig()
+{
+  if(DEBUG) console.log( "Method call: displayConfig()" );
+  console.log("------------------------------------------");
+  console.log("Settings:");       
+  var options = Settings.data();
+  console.log(JSON.stringify(options));       
+  console.log(" ");
+  console.log("Variables:");
+  console.log("Debug: " + DEBUG);
+  console.log("Version: " + VERSION);
+  console.log("default_text_color: " +  default_text_color);
+  console.log("default_background_color: " +  default_background_color);
+  console.log("config_background_color: " + config_background_color);
+  console.log("config_text_color: " + config_text_color);
+  console.log("config_splashscreen: " + config_splashscreen);
+  console.log("config_shake: " + config_shake);
+  console.log("splashscreenTimer: " + splashscreenTimer);
+  console.log("------------------------------------------");
+};
+
+
 // Prepare the accelerometer
-Accel.init();
-
-//Next Features
-//Settings - Background Color,Text color, Text style, Numbers 
-//Splashscreen
-//Icon
-//More texts
-
+if (config_shake) Accel.init();
